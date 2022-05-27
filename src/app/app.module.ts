8babfcdc7dc01726ba2360089927bf78cba74bb8
AngelGamez2021 <agamez6@unisimon.edu.co>
2022-05-27 15:40:10 -0500

    add component navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componests/header/header.component'; 
+import { BodyComponent } from './componests/body/body.component';
+import { FooterComponent } from './componests/footer/footer.component'; 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
